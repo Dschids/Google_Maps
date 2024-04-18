4324a60791efa49742b7c4c31f1e2580ef2b7a6b
Bob Dschida <rdschida@students.mchenry.edu>
2024-04-18 15:57:51 -0500

    Initial commit

---
+++ app/src/main/java/com/example/googlemaps/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.googlemaps
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.googlemaps.place.PlacesReader
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.codelabs.buildyourfirstmap.place.Place
+
+class MainActivity : AppCompatActivity() {
+
+    private val places: List<Place> by lazy {
+        PlacesReader(this).read()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            addMarkers(googleMap)
+        }
+    }
+
+    /**
+     * Adds marker representations of the places list on the provided GoogleMap object
+     */
+    private fun addMarkers(googleMap: GoogleMap) {
+        places.forEach { place ->
+            val marker = googleMap.addMarker(
+                MarkerOptions()
+                    .title(place.name)
+                    .position(place.latLng)
+            )
+        }
+    }
+
+}
